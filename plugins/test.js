204d8b8e451d81a3e400ea403762603f20894e2c
Natz396 <84192322+Natz396@users.noreply.github.com>
2022-12-13 13:33:47 +0100

    replaced listener for add_cells
    This includes helper functions:
    1) extractTreeCells
    2) determineTreeParent
    3)getTreeParentLevel

--- plugins/test.js
+++ plugins/test.js
@@ -20,55 +20,76 @@ Draw.loadPlugin(function(ui)
 	
 	
 	 graph.addListener(mxEvent.ADD_CELLS, function(sender, evt)
-	 {
+	  {
+		  //console.log('cell added');
 		var cells = evt.getProperty('cells');
-		//console.log("ich höre");
-		/*
-		var id = cells[1].getId();
-		var parent = cells[1].getParent().getId();
-		var source = cells[1].source;
-		console.log("ich höre:" + id + " parent:" + parent + " source: " + source);
-		var edges = cells[1].edges;
-		console.log("edge-ID: " + edges[0].getId() + "source: " + edges[0].source.getId());
-		var root_id = "oCXxoy5enejrwtkBi-wg-2";
-		var root = graph.model.getCell(root_id);
-		console.log(root);
-		edges_out = graph.getOutgoingEdges(root);
-		console.log("edges out:");
-		console.log(edges_out);
-		var cell_id = "oWGSHzPWtzd4GZvuny7O-4";
-		var cell = graph.model.getCell(cell_id);
-		*/
-		 
-		var added_cell = cells[1];
-		let tree_par_level = determineTreeParent(added_cell);
-		//console.log("tree parent with function: " + tree_par.getId());
-		console.log("level: " + tree_par_level);
-		changeStyle(added_cell, tree_par_level);
-		var tree_root = graph.findTreeRoots(added_cell.getParent(), false, false);
-		console.log("tree_root-ID");
-		console.log(tree_root[0].getId());
-		//graph.model.setStyle(tree_root, "connectable=0");
-
-		graph.traverse(tree_root[0], true, function (vertex, edge)
+		console.log('länge:\t' + cells.length);
+		let treeCells = extractTreeCells(cells);
+		if (treeCells.length >= 1)
 		{
-		   console.log(vertex.getId());
+			mxUtils.forEach(treeCells, function(treeCell)
+			{
+				//console.log('tree cell: ' + treeCell.getId());
+				let treeParent = determineTreeParent(treeCell);
+				console.log('ID of tree parent: ' + treeParent.getId());
+
+				let treeParentLevel = getTreeParentLevel(treeParent);
+				console.log('level of tree parent: ' + treeParentLevel);
+				setTreeOpenbisValue(treeCell, 'inventory', treeParentLevel);
+			});
+		}
+		else
+		{
+			console.log('The added cell does not belong to an openBIS tree.');
+		}
+	  });
+	  
+	 
+
+	function extractTreeCells(cells)
+	{
+		let treeCells = graph.model.filterCells(cells, function(cell)
+		{	
+			if (isOpenbisMember(cell.parent) == true && cell.isVertex() == true)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
 		});
+		return treeCells; 
+	};
 
-		
 
-	 });
+
 
 	 function determineTreeParent(cell)
 	 {
 		let edgesIn = graph.getIncomingEdges(cell);
 		let tree_parent = edgesIn[0].getTerminal(1); // getTerminal: boolean indicates if source (1) or target (0) should be determined
-		//return tree_parent;
-		
-		let level = tree_parent.getAttribute("openBIS-hierarchy");
-		return level;
-		
+		return tree_parent;
 	 };
+
+
+	 function getTreeParentLevel(cell)
+	 {
+		let openbisNodes = getOpenBIS(cell);
+		if (openbisNodes.length > 1)
+		{
+			console.log('The value of the treeParent cell with ID: ' + cell.getId() + ' ' + 'contains multiple openBIS nodes. The first one is chosen. Please make sure this is correct.');
+		}
+		else if (openbisNodes.length == 0)
+		{
+			console.log('The value of the treeParent cell with ID: ' +  cell.getId() + ' ' + 'does not contain an openBIS node. This should not happen. Please make sure this is correct.');
+		}
+		else 
+		{
+			let level = openbisNodes[0].getAttribute('treeHierarchy');
+			return level; 
+		}
+	 }
 
 
 	 function changeStyle(cell, parent_level)
