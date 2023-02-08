61792b9dffee64c2d186a4441e648d074a82686b
Natalie Augustin <n.augustin@fu-berlin.de>
2023-02-08 12:55:37 +0100

    new dialog window to add administrative metadata

--- plugins/openbis/fileStructure.js
+++ plugins/openbis/fileStructure.js
@@ -637,6 +637,603 @@ Draw.loadPlugin(function(ui)
 
 	});
 
+	// Adds resource for action
+	mxResources.parse('entityDialog=Change openBIS Data...');
+
+	var uiCreatePopupMenu = ui.menus.createPopupMenu;
+	ui.menus.createPopupMenu = function(menu, cell, evt)
+	{
+		uiCreatePopupMenu.apply(this, arguments);
+		
+		var graph = ui.editor.graph;
+		
+		if (graph.model.isVertex(graph.getSelectionCell()))
+		{
+			this.addMenuItems(menu, ['-', 'entityDialog'], null, evt);
+		}
+	};
+
+	//
+	// Main function
+	//
+	function anotherName()
+	{
+		console.log('grüßli');
+		
+	};
+	
+	// Adds action
+	ui.actions.addAction('entityDialog', function()
+	{
+		anotherName();
+		console.log('es wird');
+		var cell = graph.getSelectionCell() || graph.getModel().getRoot();
+		ui.showEntityDialog(cell);
+	}, null, null, Editor.ctrlKey + '+O');
+
+
+	EditorUi.prototype.showEntityDialog = function(cell)
+	{
+
+		if (cell != null)
+		{
+			var dlg = new EditEntityDialog(this, cell);
+			//var dlg = new EmbedDialog(ui, mxUtils.getPrettyXml(doc), null, null, null, 'Dateiname:', null, null, "openbisInitialisation.xml");
+			this.showDialog(dlg.container, 480, 420, true, false, null,false);
+			dlg.init();
+		}
+	};
+
+
+	var EditEntityDialog = function(ui, cell)
+{
+	var cellID = cell.getId();
+	console.log('cellid: ' + cell.id);
+	let oNode = getOpenBIS(cell)[0];
+	console.log(oNode.getAttribute('treeHierarchy'));
+
+	var div = document.createElement('div');
+	var graph = ui.editor.graph;
+	
+	var value = graph.getModel().getValue(cell);
+	
+	// Converts the value to an XML node
+	if (!mxUtils.isNode(value))
+	{
+		var doc = mxUtils.createXmlDocument();
+		var obj = doc.createElement('object');
+		obj.setAttribute('label', value || '');
+		value = obj;
+	}
+	
+	var meta = {};
+	
+	try
+	{
+		var temp = mxUtils.getValue(ui.editor.graph.getCurrentCellStyle(cell), 'metaData', null);
+		
+		if (temp != null)
+		{
+			meta = JSON.parse(temp);
+		}
+	}
+	catch (e)
+	{
+		// ignore
+	}
+	
+	// Creates the dialog contents
+	var form = new mxForm('entities');
+	form.table.style.width = '100%';
+
+	var attrs = value.attributes;
+	var names = [];
+	var texts = [];
+	var count = 0;
+
+	var id = (EditDataDialog.getDisplayIdForCell != null) ?
+		EditDataDialog.getDisplayIdForCell(ui, cell) : null;
+	
+	var addRemoveButton = function(text, name)
+	{
+		var wrapper = document.createElement('div');
+		wrapper.style.position = 'relative';
+		wrapper.style.paddingRight = '20px';
+		wrapper.style.boxSizing = 'border-box';
+		wrapper.style.width = '100%';
+		
+		var removeAttr = document.createElement('a');
+		var img = mxUtils.createImage(Dialog.prototype.closeImage);
+		img.style.height = '9px';
+		img.style.fontSize = '9px';
+		img.style.marginBottom = (mxClient.IS_IE11) ? '-1px' : '5px';
+		
+		removeAttr.className = 'geButton';
+		removeAttr.setAttribute('title', mxResources.get('delete'));
+		removeAttr.style.position = 'absolute';
+		removeAttr.style.top = '4px';
+		removeAttr.style.right = '0px';
+		removeAttr.style.margin = '0px';
+		removeAttr.style.width = '9px';
+		removeAttr.style.height = '9px';
+		removeAttr.style.cursor = 'pointer';
+		removeAttr.appendChild(img);
+		
+		var removeAttrFn = (function(name)
+		{
+			return function()
+			{
+				var count = 0;
+				
+				for (var j = 0; j < names.length; j++)
+				{
+					if (names[j] == name)
+					{
+						texts[j] = null;
+						form.table.deleteRow(count + ((id != null) ? 1 : 0));
+						
+						break;
+					}
+					
+					if (texts[j] != null)
+					{
+						count++;
+					}
+				}
+			};
+		})(name);
+		
+		mxEvent.addListener(removeAttr, 'click', removeAttrFn);
+		
+		var parent = text.parentNode;
+		wrapper.appendChild(text);
+		wrapper.appendChild(removeAttr);
+		parent.appendChild(wrapper);
+	};
+	
+	let openbisAttrs = oNode.attributes;
+		console.log(openbisAttrs[1].name + openbisAttrs[1].nodeValue);
+
+	var addTextArea = function(index, name, value)
+	{
+		names[index] = name;
+		texts[index] = form.addTextarea(names[count] + ':', value, 2);
+		texts[index].style.width = '100%';
+
+
+		if (value.indexOf('\n') > 0)
+		{
+			texts[index].setAttribute('rows', '2');
+		}
+		
+		addRemoveButton(texts[index], name);
+		
+		if (meta[name] != null && meta[name].editable == false)
+		{
+			texts[index].setAttribute('disabled', 'disabled');
+		}
+	};
+	
+	var temp = [];
+	var isLayer = graph.getModel().getParent(cell) == graph.getModel().getRoot();
+
+	for (var i = 0; i < attrs.length; i++)
+	{
+		if ((isLayer || attrs[i].nodeName != 'label') && attrs[i].nodeName != 'placeholders')
+		{
+			temp.push({name: attrs[i].nodeName, value: attrs[i].nodeValue});
+		}
+	}
+	
+	// Sorts by name
+	temp.sort(function(a, b)
+	{
+	    if (a.name < b.name)
+	    {
+	    	return -1;
+	    }
+	    else if (a.name > b.name)
+	    {
+	    	return 1;
+	    }
+	    else
+	    {
+	    	return 0;
+	    }
+	});
+
+	if (id != null)
+	{	
+		var text = document.createElement('div');
+		text.style.width = '100%';
+		text.style.fontSize = '11px';
+		text.style.textAlign = 'center';
+		mxUtils.write(text, id);
+		var hallo = document.createElement('div');
+		mxUtils.write(hallo, 'hallohallo');
+		
+		//var idInput = form.addField(mxResources.get('id') + ':', text);
+		var testInput = form.addField('Test:', hallo);
+		var AreaInput = form.addTextarea('Prop1:', 'halloAreaInput',2);
+		console.log(AreaInput.value);
+		console.log('read out');
+		var combotest = form.addCombo('COMBO:' ,false, 1);
+		form.addOption(combotest, 'labelOption1', 'OPTION1', false);
+		form.addOption(combotest, 'labelOption2', 'OPTION2', true);
+		form.addOption(combotest, 'labelOption3', 'OPTION3', false);
+		form.addCheckbox('namecheck', true);
+		form.addText('nameText', 'val', true)
+		var testCell = graph.model.getCell('6BlT60FBLKf2mpYVhe83-6');
+		testCell.setAttribute('testBeschreibung', AreaInput.value);
+
+
+		console.log('attrs loop');
+		openbisFormElements = [];
+		for (let i = 0; i < openbisAttrs.length; i++)
+		{
+			console.log(openbisAttrs[i].name);
+			openbisFormElements[i] = form.addTextarea(openbisAttrs[i].name, openbisAttrs[i].nodeValue,2);
+		}
+
+
+		
+		mxEvent.addListener(text, 'dblclick', function(evt)
+		{
+			if (mxEvent.isShiftDown(evt))
+			{
+				var dlg = new FilenameDialog(ui, id, mxResources.get('apply'), mxUtils.bind(this, function(value)
+				{
+					if (value != null && value.length > 0 && value != id)
+					{
+						if (graph.getModel().getCell(value) == null)
+						{
+							graph.getModel().cellRemoved(cell);
+							cell.setId(value);
+							id = value;
+							idInput.innerHTML = mxUtils.htmlEntities(value);
+							graph.getModel().cellAdded(cell);
+						}
+						else
+						{
+							ui.handleError({message: mxResources.get('alreadyExst', [value])});
+						}
+					}
+				}), mxResources.get('id'));
+				ui.showDialog(dlg.container, 300, 80, true, true);
+				dlg.init();
+			}
+		});
+
+		text.setAttribute('title', 'Shift+Double Click to Edit ID');
+	}
+	
+	for (var i = 0; i < temp.length; i++)
+	{
+		addTextArea(count, temp[i].name, temp[i].value);
+		count++;
+	}
+	
+	
+	
+	this.init = function()
+	{
+		if (texts.length > 0)
+		{
+			texts[0].focus();
+		}
+		else
+		{
+			nameInput.focus();
+		}
+	};
+	var top = document.createElement('div');
+	top.style.position = 'absolute';
+	top.style.top = '30px';
+	top.style.left = '30px';
+	top.style.right = '30px';
+	top.style.bottom = '80px';
+	top.style.overflowY = 'auto';
+	
+	top.appendChild(form.table);
+
+	var newProp = document.createElement('div');
+	newProp.style.boxSizing = 'border-box';
+	newProp.style.paddingRight = '160px';
+	newProp.style.whiteSpace = 'nowrap';
+	newProp.style.marginTop = '6px';
+	newProp.style.width = '100%';
+	
+	var nameInput = document.createElement('input');
+	nameInput.setAttribute('placeholder', mxResources.get('enterPropertyName'));
+	nameInput.setAttribute('type', 'text');
+	nameInput.setAttribute('size', (mxClient.IS_IE || mxClient.IS_IE11) ? '36' : '40');
+	nameInput.style.boxSizing = 'border-box';
+	nameInput.style.marginLeft = '2px';
+	nameInput.style.width = '100%';
+	
+	//newProp.appendChild(nameInput);
+	top.appendChild(newProp);
+	div.appendChild(top);
+
+	
+	var addBtn = mxUtils.button(mxResources.get('addProperty'), function()
+	{
+		var name = nameInput.value;
+
+		// Avoid ':' in attribute names which seems to be valid in Chrome
+		if (name.length > 0 && name != 'label' && name != 'placeholders' && name.indexOf(':') < 0)
+		{
+			try
+			{
+				var idx = mxUtils.indexOf(names, name);
+				
+				if (idx >= 0 && texts[idx] != null)
+				{
+					texts[idx].focus();
+				}
+				else
+				{
+					// Checks if the name is valid
+					var clone = value.cloneNode(false);
+					clone.setAttribute(name, '');
+					
+					if (idx >= 0)
+					{
+						names.splice(idx, 1);
+						texts.splice(idx, 1);
+					}
+
+					names.push(name);
+					var text = form.addTextarea(name + ':', '', 2);
+					text.style.width = '100%';
+					texts.push(text);
+					addRemoveButton(text, name);
+
+					text.focus();
+				}
+
+				addBtn.setAttribute('disabled', 'disabled');
+				nameInput.value = '';
+			}
+			catch (e)
+			{
+				mxUtils.alert(e);
+			}
+		}
+		else
+		{
+			mxUtils.alert(mxResources.get('invalidName'));
+		}
+	});
+	
+	
+	this.init = function()
+	{
+		if (texts.length > 0)
+		{
+			texts[0].focus();
+		}
+		else
+		{
+			nameInput.focus();
+		}
+	};
+	
+	addBtn.setAttribute('title', mxResources.get('addProperty'));
+	addBtn.setAttribute('disabled', 'disabled');
+	addBtn.style.textOverflow = 'ellipsis';
+	addBtn.style.position = 'absolute';
+	addBtn.style.overflow = 'hidden';
+	addBtn.style.width = '144px';
+	addBtn.style.right = '0px';
+	addBtn.className = 'geBtn';
+	//newProp.appendChild(addBtn);
+
+	var cancelBtn = mxUtils.button(mxResources.get('cancel'), function()
+	{
+		ui.hideDialog.apply(ui, arguments);
+	});
+	
+	cancelBtn.className = 'geBtn';
+	
+	var applyBtn = mxUtils.button(mxResources.get('apply'), function()
+	{
+		try
+		{
+
+		
+			console.log('on apply');
+
+
+			ui.hideDialog.apply(ui, arguments);
+			
+			// Clones and updates the value
+			value = value.cloneNode(true);
+			console.log(value);
+			console.log(AreaInput.value);
+			var removeLabel = false;
+			
+			for (var i = 0; i < names.length; i++)
+			{
+				console.log('before set Attr');
+				console.log(texts[i].value);
+				if (texts[i] == null)
+				{
+					value.removeAttribute(names[i]);
+				}
+				else
+				{
+					value.setAttribute(names[i], texts[i].value);
+					value.setAttribute('complNew', AreaInput.value);
+					console.log(texts[i].value);
+					removeLabel = removeLabel || (names[i] == 'placeholder' &&
+						value.getAttribute('placeholders') == '1');
+				}
+			}
+
+			var oNodeNew = oNode.cloneNode(true);
+			for (var i = 0; i < openbisAttrs.length; i++)
+			{
+				oNodeNew.setAttribute(openbisAttrs[i].name, openbisFormElements[i].value);
+				//console.log(openbisAttrs[i].name );
+				//console.log(openbisFormElements[i].value);
+			}
+			console.log('before replacement');
+		
+			console.log('after replace');
+			console.log(value.childNodes);
+
+			for (let i = 0; i < value.childNodes.length; i++)
+			{
+				console.log(value.childNodes[i].tagName);
+				if(value.childNodes[i].tagName == 'openbis')
+				{
+					value.removeChild(value.childNodes[i]);
+					console.log('true');
+				}
+			}
+			console.log('openbischilds');
+			console.log(value.childNodes);
+			value.appendChild(oNodeNew);
+			console.log(value.childNodes);
+
+
+			//console.log(oNodeNew);
+			//console.log(oNode.parentNode.nodeName);
+			//value.removeChild(oNode);
+
+			// Removes label if placeholder is assigned
+			if (removeLabel)
+			{
+				value.removeAttribute('label');
+			}
+			
+			// Updates the value of the cell (undoable)
+			graph.getModel().setValue(cell, value);
+			console.log(value);
+		}
+		catch (e)
+		{
+			mxUtils.alert(e);
+		}
+	});
+	applyBtn.className = 'geBtn gePrimaryBtn';
+	
+	function updateAddBtn()
+	{
+		if (nameInput.value.length > 0)
+		{
+			addBtn.removeAttribute('disabled');
+		}
+		else
+		{
+			addBtn.setAttribute('disabled', 'disabled');
+		}
+	};
+
+	mxEvent.addListener(nameInput, 'keyup', updateAddBtn);
+	
+	// Catches all changes that don't fire a keyup (such as paste via mouse)
+	mxEvent.addListener(nameInput, 'change', updateAddBtn);
+	
+	var buttons = document.createElement('div');
+	buttons.style.cssText = 'position:absolute;left:30px;right:30px;text-align:right;bottom:30px;height:40px;'
+	
+	if (ui.editor.graph.getModel().isVertex(cell) || ui.editor.graph.getModel().isEdge(cell))
+	{
+		var replace = document.createElement('span');
+		replace.style.marginRight = '10px';
+		var input = document.createElement('input');
+		input.setAttribute('type', 'checkbox');
+		input.style.marginRight = '6px';
+		
+		if (value.getAttribute('placeholders') == '1')
+		{
+			input.setAttribute('checked', 'checked');
+			input.defaultChecked = true;
+		}
+	
+		mxEvent.addListener(input, 'click', function()
+		{
+			if (value.getAttribute('placeholders') == '1')
+			{
+				value.removeAttribute('placeholders');
+			}
+			else
+			{
+				value.setAttribute('placeholders', '1');
+			}
+		});
+		
+		/*replace.appendChild(input);
+		mxUtils.write(replace, mxResources.get('placeholders'));
+		
+		if (EditDataDialog.placeholderHelpLink != null)
+		{
+			var link = document.createElement('a');
+			link.setAttribute('href', EditDataDialog.placeholderHelpLink);
+			link.setAttribute('title', mxResources.get('help'));
+			link.setAttribute('target', '_blank');
+			link.style.marginLeft = '8px';
+			link.style.cursor = 'help';
+			
+			var icon = document.createElement('img');
+			mxUtils.setOpacity(icon, 50);
+			icon.style.height = '16px';
+			icon.style.width = '16px';
+			icon.setAttribute('border', '0');
+			icon.setAttribute('valign', 'middle');
+			icon.style.marginTop = (mxClient.IS_IE11) ? '0px' : '-4px';
+			icon.setAttribute('src', Editor.helpImage);
+			link.appendChild(icon);
+			
+			replace.appendChild(link);
+		}
+		*/
+		
+		buttons.appendChild(replace);
+	}
+	
+	if (ui.editor.cancelFirst)
+	{
+		buttons.appendChild(cancelBtn);
+		buttons.appendChild(applyBtn);
+	}
+	else
+	{
+		buttons.appendChild(applyBtn);
+		buttons.appendChild(cancelBtn);
+	}
+
+	div.appendChild(buttons);
+
+
+
+	
+	this.container = div;
+};
+
+
+
+	/* vertex wird kurz hinzugefügt und dann ist es unsichtbar
+	var parent = ui.editor.graph.getDefaultParent();
+    var model = graph.getModel();
+	var index = model.getChildCount(parent);
+	var v1 = graph.insertVertex(graph.model.getCell('1'), null, 'Doubleclick', 20, 20, 80, 30);
+	graph.getModel().beginUpdate();
+	try
+	{
+		console.log('tadaaaaaa');
+		model.add(parent, v1,index);
+	}
+	finally
+	{
+		graph.getModel().endUpdate();
+	}
+
+	graph.model.setStyle(v1, 'fillColor=#00FF80;fillOpacity=100;');
+	*/
+
 	
 	/*
 	var parent = ui.editor.graph.getDefaultParent();
